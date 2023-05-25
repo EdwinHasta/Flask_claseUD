638148238f2a3a1901ffecccffcc40dbcbc448b0
EdwinHasta <igni2086@gmail.com>
2023-05-25 15:20:26 -0500

    adicion de la plantilla

--- proyecto/centroDeServicio/home/main.py
+++ proyecto/centroDeServicio/home/main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for
+import random
+
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-    return 'hello'
+    return render_template('index.html')
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
